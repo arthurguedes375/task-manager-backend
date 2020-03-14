b6f53ae5aacb907d321ba9d9210c080aaea16ab6
Arthur Guedes <arthurguedes375@gmail.com>
2020-03-14 15:24:30 -0300

    Modal

---
+++ frontend/assets/js/edit.js
@@ -0,0 +1,24 @@
+var query = location.search.slice(1);
+var partes = query.split('&');
+var data = {};
+partes.forEach(function (parte) {
+    var chaveValor = parte.split('=');
+    var chave = chaveValor[0];
+    var valor = chaveValor[1];
+    data[chave] = valor;
+});
+
+// console.log(data.id); 
+
+$.ajax({
+    type: "GET",
+    url: "http://localhost/taskmanager/backend/task/edit",
+    data: {
+        id: data.id
+    },
+    dataType: "json",
+    success: function (response) {
+        console.log(response);
+        
+    }
+});
